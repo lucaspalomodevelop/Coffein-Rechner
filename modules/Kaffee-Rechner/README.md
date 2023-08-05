44418a9e58f621630f2aa50118e2c7ea7ab17fa9
lucaspalomodevelop <lucas.palomo@t-online.de>
2023-08-05 16:30:46 +0200

    delete kaffee-Rehcner code

--- modules/Kaffee-Rechner/README.md
+++
@@ -1,9 +0,0 @@
-# Mate-Rechner
-Berechnung des idealen Mate Konsums
-
-## Benutzen
-
-* compile: `make`
-* clean: `make clean`
-* run: `./mate`
-* install: `make install`
--- modules/Kaffee-Rechner/coffee.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef MATE_H
-#define MATE_H
-
-#include <iostream>
-extern int kaffee, Koffeingehalt, marke, kmenge, ergebnis;
-extern std::string input, markenname;
-#endif
